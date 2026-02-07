53e86985b41e4fecb87822a5a7bff11231f38bc7
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2026-02-07 22:14:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,24 +3,7 @@ provider "aws" {
  
 
 }
-data "aws_ami" "myami"{
 
-owners = ["amazon"]
-most_recent = true
-
-filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-
-}
-
-resource "aws_instance" "myec2" {
-  ami    = data.aws_ami.myami.id 
-  instance_type = "t2.nano"
-
-}
 
 
  
